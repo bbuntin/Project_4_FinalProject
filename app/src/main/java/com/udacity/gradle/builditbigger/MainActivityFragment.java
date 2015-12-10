a8ca7b7b4e01297fb90fdca41a7d073b00e2d413
Brad Buntin <bradley4@gmail.com>
2015-12-09 23:44:06 -0500

    add java project

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,11 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.gmail.bradley4.gradle.jokes.Joker;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
@@ -22,7 +24,9 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
-
+        TextView textView = (TextView) root.findViewById(R.id.joke);
+        Joker myJoker = new Joker();
+        textView.setText(myJoker.getJoke());
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
---
+++ javajokes/src/main/java/com/gmail/bradley4/gradle/jokes/Joker.java
@@ -0,0 +1,10 @@
+package com.gmail.bradley4.gradle.jokes;
+
+/**
+ * Created by Bradley on 12/9/15.
+ */
+public class Joker {
+    public String getJoke(){
+        return "This is totally a funny joke";
+    }
+}
