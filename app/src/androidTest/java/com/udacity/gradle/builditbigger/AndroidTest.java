c60f5aec1759b85dcca2ad1ff34813b60748d243
Brad Buntin <bradley4@gmail.com>
2015-12-23 00:10:45 -0500

    added test project

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AndroidTest.java
@@ -0,0 +1,14 @@
+package com.udacity.gradle.builditbigger;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by Bradley on 12/20/15.
+ */
+
+public class AndroidTest extends AndroidTestCase {
+    public void testAsyncTask(){
+        //assertEquals("hello", "hello");
+        if (1 ==1 ) throw new AssertionError();
+    }
+}
---
+++ test/src/main/java/com/example/EchoTest.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.junit.Test;
+
+public class EchoTest {
+    @Test
+    public void verifyEchoResponse(){
+        assert false;
+    }
+}
