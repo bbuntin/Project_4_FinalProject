927952cfdc06127312f4091a8641628a6850f87d
Brad Buntin <bradley4@gmail.com>
2015-12-31 11:59:33 -0500

    working paid and free versions

---
+++ app/src/freeFlavor/java/com/udacity/gradle/builditbigger/Constants.java
@@ -0,0 +1,13 @@
+package com.udacity.gradle.builditbigger;
+
+/**
+ * Created by Bradley on 12/31/15.
+ */
+public class Constants {
+
+    public enum Type {
+        FREE, PAID;
+    }
+
+    public static final Constants.Type type = Type.FREE;
+}
