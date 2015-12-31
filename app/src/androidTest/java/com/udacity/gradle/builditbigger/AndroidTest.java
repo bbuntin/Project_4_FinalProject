91aa5ec310975916df7e76c789722c5f67d35072
Brad Buntin <bradley4@gmail.com>
2015-12-31 09:46:47 -0500

    able to wait for async from test project now.

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AndroidTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AndroidTest.java
@@ -3,8 +3,9 @@ package com.udacity.gradle.builditbigger;
 import android.content.Context;
 import android.test.AndroidTestCase;
 import android.test.mock.MockContext;
-import android.util.Log;
 import android.util.Pair;
+
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by Bradley on 12/20/15.
@@ -12,11 +13,22 @@ import android.util.Pair;
 
 public class AndroidTest extends AndroidTestCase {
     Context context;
+    private CountDownLatch signal;
+
     public void testAsyncTask(){
         //assertEquals("hello", "hello");
-        Log.e("buntin", "really good");
+        signal = new CountDownLatch(1);
+
+        //Log.e("buntin", "really good");
         context = new MockContext();
         new com.udacity.gradle.builditbigger.EndpointsAsyncTask().execute(new Pair<Context, String>(context, "Nothing"));
+
+        try{
+            signal.await();
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+
         if (1 ==1 ) throw new AssertionError();
     }
 }
